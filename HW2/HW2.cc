b9731cf58f45ba1ee0866a0955443ac3d15653ca
FarhanAxiq <angahmyone@aol.com>
2020-09-23 15:27:19 -0600

    Welp, almost there, need to fix the game logic

--- HW2/HW2.cc
+++ HW2/HW2.cc
@@ -12,7 +12,8 @@
 #include <fstream>   // std::ifstream
 #include <string>    // std::getline
 #include <vector>    // std::vector
-using _2D_Vect = std::vector<std::vector<char>>; // simplified the convoluted 2D vect name
+// alias for our 2D vector
+using _2D_Vect = std::vector<std::vector<char> >; 
 
 //
 //
@@ -28,24 +29,86 @@ std::ostream &operator<<(std::ostream& out, _2D_Vect &board)
     return out;
 }
 
+
+
+//
+//  Check if the board is valid
+//  Where:
+//  the input is not at least 2×2
+//  the input contains a bad character
+//  the input has inconsistent line lengths.
+//
+bool is_valid(_2D_Vect &board)
+{
+    for (std::vector<char> &row : board){
+        // check if board is at least 2x2 or inconsistent length
+        if  (row.size() < 2 || board.size() < 2 || row.size() != board.at(0).size()){ return false; } 
+        // check if the input is a valid char
+        for (char &c : row) { 
+            if (c != '.' && c != 'O' && c != isspace(c)) { return false; } 
+        } 
+    }
+    return true;
+}
+
+//
+//
+//  check and count living neighbor
+//
+//
+int get_count(_2D_Vect &board, int &x, int &y, int &row, int &col)
+{
+    int count = 0;
+    for (int i = -1; i < 2; i++){
+        for (int j = -1; j < 2; j++){
+            int n_row = (x + i + row) % row;
+            int n_col = (y + j + col) % col;
+            // if ourself continue
+            // if neighbor alive, count++
+            if (i == x && j == x) { continue; }
+            else if (board.at(n_row).at(n_col) == 'O') { count++; }
+        }
+    }
+    return count;
+}
+
+//
+// Game Rules: 
+// 1) Any live cell with 0 or 1 live neighbors becomes dead, because of underpopulation
+// 2) Any live cell with 2 or 3 live neighbors stays alive, because its neighborhood is just right
+// 3) Any live cell with more than 3 live neighbors becomes dead, because of overpopulation
+// 4) Any dead cell with exactly 3 live neighbors becomes alive, by reproduction
+//
 void game(_2D_Vect &board)
 {
-    bool state;
-    char cells;
+    const char dead  = '.';
+    const char alive = 'O';
+    int row = board.size();
+    int col = board.at(0).size(); // why at 0? Because is_valid has confirmed that there's no inconsistent length line
+    // initialize next board
+    _2D_Vect next_board{(size_t)row, std::vector<char>(col, dead)}; 
     // traverse the vector
-    for (std::vector<char> &row : board) {
-        for (char &c : row) {
-            std::cout << c;
+    // gonna be a long code here 
+    // TODO: investigate why wrong output
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < col; j++){
+            int life_count = get_count(board, i, j, row, col);
+            char state = board.at(i).at(j);
+            if      (state == dead && life_count == 3) { next_board.at(i).at(j) = alive; }
+            else if (state == alive && (life_count < 2 || life_count > 3)) { next_board.at(i).at(j) = dead; }
+            else    { next_board.at(i).at(j) = state; }
         }
-
-        std::cout << '\n';
     }
+    std::cout << next_board;
 }
+
 
 //
 //
 //  Create a vector for the game
 //  from the file
+//  thus creating the initial state
+//  of the board.
 //
 //
 _2D_Vect create_board(std::string filename)
@@ -63,18 +126,27 @@ _2D_Vect create_board(std::string filename)
         for (char &c : line){ rows.emplace_back(c); }
         board.emplace_back(rows);
     }
+    // if the board generated isn't valid
+    // exit
+    // non-valid board is definind in is_valid() function
+    if (!is_valid(board)){
+        std::cerr << "The board generated from " << filename << " is not a valid board.\n";
+        exit(1);
+    }
+    
     in.close();
     return board;
 }
 
+
 int main(int argc, char *argv[])
 {
-    if (argc < 1){
+    if (argc < 2){
         std::cerr << "No argument is passed!\n";
         exit(1);
     }
     
-    // make a 2D vector, see typedef for full type definition
+    // make a 2D vector, see using for full type definition
     _2D_Vect board = create_board(argv[1]);
     game(board);
     return 0;
